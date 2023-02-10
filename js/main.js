62b91efd63389e9d81c4db7c7ce93219b165b246
msingh17122000 <66904586+msingh17122000@users.noreply.github.com>
2023-02-10 01:22:32 -0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function followUser(followbtn){
+    followbtn.innerHTML="Following";
+}
+function clickedbtnlike(btn){
+    btn.innerHTML="<img src='assets/heartclicked.png'class='btn-images'>";
+
+}
+function clickedbtnsend(btn){
+    btn.innerHTML="<img src='assets/sendclicked.png'class='btn-images'>";
+}
